e7a16c134db2d49bdb267df840ad00883c71011d
VaishnaviPawar07 <vaishnupawar2503@gmail.com>
2026-06-08 23:54:31 +0530

    Voting Eligibility Checker

---
+++ Voting_Eligibility.py
@@ -0,0 +1,8 @@
+#Voting Eligibility Checker
+
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You are eligible for voting")
+else:
+    print("You are not eligible for voting")
