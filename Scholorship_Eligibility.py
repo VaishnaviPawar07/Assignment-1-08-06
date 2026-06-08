0d613a72c9c951c583b42f2b2cde9b84220335e1
VaishnaviPawar07 <vaishnupawar2503@gmail.com>
2026-06-09 00:56:24 +0530

    Scholarship Eligibility

---
+++ Scholorship_Eligibility.py
@@ -0,0 +1,8 @@
+#Scholarship Eligibility
+name=input("Enter your name:")
+percentage = float(input("Enter your percentage: "))
+
+if percentage >= 90:
+    print(f"{name} you are eligible for Scholarship")
+else:
+    print(f"{name} you are not eligible for Scholarship")
