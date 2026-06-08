dfb837780b5546f5386a73bef4ad01f3a9e7d702
VaishnaviPawar07 <vaishnupawar2503@gmail.com>
2026-06-09 01:06:49 +0530

    Student Research system students

---
+++ Stud_Research_System.py
@@ -0,0 +1,9 @@
+#Student Research system
+students = ["Vaishnu", "Rutvik", "Nishi", "Ankita"]
+
+name = input("Enter student name: ")
+
+if name in students:
+    print("Student found")
+else:
+    print("Student not found")
