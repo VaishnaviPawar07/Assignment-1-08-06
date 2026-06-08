b17a11a8272748ac236c77fa840ebbfaaba532fa
VaishnaviPawar07 <vaishnupawar2503@gmail.com>
2026-06-08 21:22:50 +0530

    Simple Calculator Program

---
+++ Simple Cal.py
@@ -0,0 +1,14 @@
+#Simple Calcy
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+
+#arithmetic
+
+print(f"Addition of {num1} and {num2} is",num1+num2)
+print(f"subtraction of {num1} and {num2} is ", num1-num2)
+print(f"multiplication of {num1} and {num2} is ", num1*num2)
+print(f"Division of {num1} and {num2} is", num1/num2)
+print(f"remainder of {num1} and {num2} is", num1%num2)
+print(f"exponent of {num1} and {num2} is", num1**num2)575
+
+
