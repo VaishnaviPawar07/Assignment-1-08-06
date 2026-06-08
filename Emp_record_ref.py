00fa885668be1213d7cfb6cd43915708121bd0c2
VaishnaviPawar07 <vaishnupawar2503@gmail.com>
2026-06-09 01:32:18 +0530

    Emp_record_ref

---
+++ Emp_record_ref.py
@@ -0,0 +1,8 @@
+#Emp_record_ref
+employee1 = {"id":75, "name":"vaishnu"}
+employee2 = employee1
+
+if employee1 is employee2:
+    print("Same employee record")
+else:
+    print("Different employee records")
